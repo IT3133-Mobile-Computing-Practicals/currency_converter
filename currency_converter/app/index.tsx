9accb3b715187b25d3a7ca9d7b082de8d9fc41f5
TharinduLakshimal <tharindulakshimal@gmail.com>
2024-12-23 21:57:01 +0530

    feat:create basic structure

---
+++ currency_converter/app/index.tsx
@@ -0,0 +1,39 @@
+import { Text, View,Image,StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#008080",
+      }}
+    >
+      <Text style={styles.txt}>Hello World</Text>
+      <Text style={styles.subtxt}>My first ReactNative cross platform app</Text>
+    </View>
+  );
+}
+const styles= StyleSheet.create({
+  logo:{
+    width:120,
+    height:120,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'lightblue',
+    marginBottom: 20,
+  },
+  txt:{
+    fontSize:32,
+    color:"#4B0082",
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  subtxt:{
+    fontSize: 18, 
+    color: '#8B4513',
+    textAlign: 'center',
+  },
+})
